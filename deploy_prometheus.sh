0fa8956ecbb415be395742e163f62c9d6109d4cf
Andy Clemenko <clemenko@gmail.com>
2018-01-24 12:16:20 -0500

    updates

--- deploy_prometheus.sh
+++ deploy_prometheus.sh
@@ -7,6 +7,7 @@ NORMAL=$(tput sgr0)
 NODE_TARGETS=""
 ENGINE_TARGETS=""
 CADVISOR_TARGETS=""
+password=Pa22word
 
 #error checking for client bundle
 if [ -z $DOCKER_HOST ]; then
@@ -14,13 +15,8 @@ if [ -z $DOCKER_HOST ]; then
   exit 1
 fi
 
-if [[ $(docker config ls | grep service.prometheus.conf | wc -l) = 1 ]]; then
-  echo "$RED" "You already have prometheus deployed..." "$NORMAL"
-  exit 1
-fi
-
 #get node list
-echo -n " getting node list - "
+echo -n " getting node list "
 node_list=$(for NODE in $(docker node ls --format '{{.Hostname}}'); do echo -n "$(docker node inspect --format '{{.Status.Addr}}' "${NODE}") "; done; echo "")
 
 # load images
@@ -31,9 +27,8 @@ for node in ${node_list}; do
 done
 echo "$GREEN" "[ok]" "$NORMAL"
 
-
-echo -n " creating prometheus config - "
-cat << EOF | docker config create service.prometheus.conf -
+echo -n " creating prometheus config file "
+cat << EOF >> service.prometheus.conf
 global:
   scrape_interval:     15s
   evaluation_interval: 15s
@@ -44,10 +39,10 @@ scrape_configs:
     static_configs:
       - targets: [${NODE_TARGETS}]
 
-  - job_name: 'nodeengine'
-    scrape_interval: 5s
-    static_configs:
-      - targets: [${ENGINE_TARGETS}]
+#  - job_name: 'nodeengine'
+#    scrape_interval: 5s
+#    static_configs:
+#      - targets: [${ENGINE_TARGETS}]
 
   - job_name: 'cadvisor'
     scrape_interval: 5s
@@ -61,19 +56,38 @@ scrape_configs:
 EOF
 echo "$GREEN" "[ok]" "$NORMAL"
 
+if [[ "$(docker config ls | grep service.prometheus.conf | wc -l|sed 's/ //g')" = 1 ]]; then
+  echo "$RED" " Updating the current config" "$NORMAL"
+
+  echo -n " creating the new docker config "
+  docker config create new.service.prometheus.conf service.prometheus.conf > /dev/null 2>&1
+  echo "$GREEN" "[ok]" "$NORMAL"
+
+  docker service update --config-rm service.prometheus.conf --config-add source=new.service.prometheus.conf,target=/etc/prometheus/prometheus.yml prometheus_prometheus
+
+  exit
+fi
+
+echo -n " creating the docker config "
+docker config create service.prometheus.conf service.prometheus.conf > /dev/null 2>&1
+echo "$GREEN" "[ok]" "$NORMAL"
+
 echo -n " deploying stack "
 docker stack deploy -c prometheus.yml prometheus
 echo "$GREEN" "[ok]" "$NORMAL"
 
-echo -n " waiting for grafana"
+echo -n " waiting for grafana to start "
 sleep 30
 
 echo -n " confgiuring grafana through the api "
-curl -skX POST  http://admin:Pa22word@app.dockr.life:3000/api/datasources -H 'Content-Type: application/json' -d "{ \"name\": \"prometheus\",\"type\": \"prometheus\",\"Access\": \"proxy\",\"url\": \"http://prometheus:9090\",\"basicAuth\": false }" > /dev/null 2>&1
+curl -skX POST  http://admin:$password@app.dockr.life:3000/api/datasources -H 'Content-Type: application/json' -d "{ \"name\": \"prometheus\",\"type\": \"prometheus\",\"Access\": \"proxy\",\"url\": \"http://prometheus:9090\",\"basicAuth\": false }" > /dev/null 2>&1
 
-curl -skX POST http://admin:Pa22word@app.dockr.life:3000/api/dashboards/import -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -d @cluster.json > /dev/null 2>&1
+curl -skX POST http://admin:$password@app.dockr.life:3000/api/dashboards/import -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -d @cluster.json > /dev/null 2>&1
 
-curl -skX POST http://admin:Pa22word@app.dockr.life:3000/api/dashboards/import -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -d @containers.json > /dev/null 2>&1
+curl -skX POST http://admin:$password@app.dockr.life:3000/api/dashboards/import -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -d @containers.json > /dev/null 2>&1
+
+curl -skX PUT http://admin:$password@app.dockr.life:3000/api/org/preferences -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -d '{"theme":"light","homeDashboardId":0,"timezone":""}' > /dev/null 2>&1
+
 echo "$GREEN" "[ok]" "$NORMAL"
 
 echo " grafana's login : admin / Pa22word "
